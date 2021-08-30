c392a60ddad859ad13efe9c67075a56632b52e22
14rau <patrickrau95@gmail.com>
2021-08-30 15:52:54 +0200

    add about

---
+++ src/commands/about.ts
@@ -0,0 +1,36 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { Client, CommandInteraction, MessageActionRow, MessageButton, MessageEmbed, Permissions } from "discord.js";
+import { Command } from ".";
+
+
+export const command: Command = {
+    data: new SlashCommandBuilder()
+        .setName("about")
+        .setDescription("Show some informations about eula"),
+        action: (interaction: CommandInteraction, client: Client) => {
+            const exampleEmbed = new MessageEmbed()
+                .setColor("#0099ff")
+                .setTitle("Eula - about")
+                .setDescription("Eula is a simple message deletion bot. You can filter for specific words, regex or generally disable URL postings. In case you just want the message from someone be deleted, i can also make Eula do this!")
+                const row = new MessageActionRow()
+                    .addComponents(
+                        new MessageButton()
+                            .setLabel("Invite")
+                            .setStyle("LINK")
+                            .setURL("https://discord.com/api/oauth2/authorize?client_id=880880243873832990&permissions=2147493888&scope=bot"),
+                        new MessageButton()
+                            .setLabel("Support")
+                            .setStyle("LINK")
+                            .setURL("https://discord.gg/Zmtr88WBBx"),
+                    );
+
+        interaction.reply({
+             embeds: [ exampleEmbed ],
+             components: [ row ],
+             ephemeral: true,
+        });
+    },
+    permissions: [ "ADMINISTRATOR" ],
+}
+
+
