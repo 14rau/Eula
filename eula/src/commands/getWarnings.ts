4476e5c7bad7d6964870ec64899312e099bd8ca5
14rau <patrickrau95@gmail.com>
2021-09-16 16:43:44 +0200

    get warnings

--- eula/src/commands/getWarnings.ts
+++ eula/src/commands/getWarnings.ts
@@ -1,4 +1,7 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
+import { MessageEmbed } from 'discord.js';
+import { Warning } from 'eula_db/dist/entity/Warning';
+import { DateTime } from 'luxon';
 import { Command } from '.';
 
 
@@ -13,6 +16,15 @@ export const command: Command = {
         .setDescription('Sets the warning threshold for your guild'),
     action: async ({interaction, eulaDb, log}) => {
         const warnings = await eulaDb.warningClient.getWarning(interaction.options.getUser("user").id, interaction.guildId)
+        
+        function map(warning: Warning): MessageEmbed {
+            return new MessageEmbed()
+            .setDescription(warning.reason)
+        }
+        
+        interaction.reply({
+            embeds: warnings.map(map)
+        })
     },
     permissions: [ "KICK_MEMBERS" ],
 }
